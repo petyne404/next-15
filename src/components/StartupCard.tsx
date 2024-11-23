09891016b4962ff6b7a71b38776cb2e182310a74
tharathep <tharathep.dev@gmail.com>
2024-11-23 10:01:06 +0700

    first commit

---
+++ src/components/StartupCard.tsx
@@ -0,0 +1,69 @@
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const StartupCard = ({ post }: { post: StartupCardType }) => {
+  const {
+    _id,
+    author: { _id: authorId, name: authorName },
+    title,
+    category,
+    description,
+    image,
+    views,
+    _createdAt,
+  } = post;
+
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary" />
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+      <div className="flex-between mt-5 gap-5">
+        <div className="felx-1">
+          <Link href={`/user/${authorId}`} className="startup_card_title">
+            <p className="text-16-medium line-clamp-1">{authorName}</p>
+          </Link>
+          <Link href={`startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+        <Link href={`/user/${authorId}`}>
+          <Image
+            src="https://placehold.co/48x48"
+            alt="startup"
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        </Link>
+      </div>
+      <Link href={`startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+        <Image
+          src={image}
+          alt="startup"
+          width={400}
+          height={400}
+          className="startup-card_img"
+        />
+      </Link>
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className="text-16-meduim">{category}</p>
+        </Link>
+        <Button className='startup-card_btn' asChild>
+            <Link href={`/startup/${_id}`}>Detail</Link>
+        </Button>
+      </div>
+    </li>
+  );
+};
+
+export default StartupCard;
